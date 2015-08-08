4e5f50d9520be118e07d598f23332d0a97e99b8c
arsarm1 <armen.s.arslanian@gmail.com>
2015-08-08 00:13:12 -0700

    added a function to test.c

--- test.c
+++ test.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-	printf("Armen Arslanian");
+	printf("Armen Arslanian\n");
+	printf("5+5=%d\n",add(5,5));
 	return 0;
+}
+/* Add two numbers
+ */
+int add(int n1,int n2)
+{
+	return n1+n2;
 }
